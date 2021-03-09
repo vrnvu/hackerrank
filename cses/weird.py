72490aecc04efec287e9fee14b1ef73b24066612
arnau <diazarnau0@gmail.com>
2021-03-09 15:23:33 +0100

    weird

--- cses/weird.py
+++ cses/weird.py
@@ -1,1 +1,13 @@
-# https://cses.fi/problemset/task/1068/
+# https://cses.fi/problemset/task/1068
+
+def weird(n: int):
+    while n != 1:
+        print(n, end=' ')
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    print(1, end=' ')
+
+n = int(input())
+weird(n)
