c2fdf4a0d9c7c995c6872d4c7e2a3afffd72d867
Arnau Diaz <diazarnau0@gmail.com>
2021-03-10 20:35:30 +0100

    wip

---
+++ hackerrank/day-of-programmer.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year):
+    return 10
+
+r = dayOfProgrammer(2017)
+print(r)
+r = dayOfProgrammer(2016)
+print(r)
+r = dayOfProgrammer(1800)
+print(r)
